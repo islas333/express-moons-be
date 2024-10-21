083a4f2a472c122cc54d59ba45244a2720e03be0
Humberto Islas <islas333@gmail.com>
2024-10-21 17:46:35 -0600

    Commit 1

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ["dist/**", "node_modules/**", "build/**", "package.json"], // Ignorar las carpetas y archivos especificados
+  },
+  ...compat.extends(
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ),
+  {
+    languageOptions: {
+			parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      eqeqeq: ["error", "always"],
+			"no-empty-function": "error",
+      "no-implicit-coercion": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-duplicate-enum-values": "error",
+      "@typescript-eslint/no-inferrable-types": "off",
+    },
+  },
+];
