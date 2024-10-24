aa06edf7dd5b39bb6132a5f6f620b3848cedf8b5
Humberto Islas <islas333@gmail.com>
2024-10-24 09:55:16 -0600

    CORS add

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import smileCenterRoutes from "./routes/smileCenterRoutes";
 
@@ -7,6 +8,7 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", smileCenterRoutes);
 
